399ab165dc9611fff682b46018f0dba88f9f4661
Oleg Kosarev <OlegKosarev502@outlook.com>
2019-11-09 18:19:09 +0300

    Created basic components

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <TodoList 
+      v-bind:todos="todos"
+    />
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList';
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      todos: [
+        { id: 1, title: 'First item', isCompleted: false },
+        { id: 2, title: 'Second item', isCompleted: false },
+        { id: 3, title: 'Third item', isCompleted: false },
+      ]
+    }
+  },
+  components: {
+    TodoList
+  }
+}
+</script>
+
+<style>
+#app { }
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ul>
+      <TodoItem 
+        v-for="todo in todos"
+        v-bind:todo="todo"
+        v-bind:key="todo.key"
+      />
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem';
+
+export default {
+  props: ['todos'],
+  components: {
+    TodoItem
+  }
+}
+</script>
