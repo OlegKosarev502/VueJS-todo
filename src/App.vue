dc6f1428572d8d063d48bd19c06d20f8d371a13f
Oleg Kosarev <OlegKosarev502@outlook.com>
2019-11-12 01:42:28 +0300

    Added routing

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,11 @@
 <template>
   <div id="app">
-    <TodoList 
-      v-bind:todos="todos"
-    />
+    <h1>Todo application</h1>
+    <hr>
+
+    <router-view />
   </div>
 </template>
-
-<script>
-import TodoList from './components/TodoList';
-
-export default {
-  name: 'app',
-  data() {
-    return {
-      todos: [
-        { id: 1, title: 'First item', isCompleted: false },
-        { id: 2, title: 'Second item', isCompleted: false },
-        { id: 3, title: 'Third item', isCompleted: false },
-      ]
-    }
-  },
-  components: {
-    TodoList
-  }
-}
-</script>
 
 <style>
 #app { }
---
+++ src/views/Todos.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <TodoList 
+      v-bind:todos="todos"
+    />
+    <router-link to="/">Home</router-link>
+  </div>
+</template>
+
+<script>
+import TodoList from '../components/TodoList';
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      todos: []
+    }
+  },
+  mounted() {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(json => { this.todos = json });
+  },
+  components: {
+    TodoList
+  }
+}
+</script>
